04f6906704a7bd6be54b12d8f78c919da38fe691
Salvatore Patisso <120101001+Salion0@users.noreply.github.com>
2025-07-08 12:21:43 +0200

    implemented rag system
    Co-authored-by: giutar-ey <giutar-ey@users.noreply.github.com>
    Co-authored-by: Matthew E. Thomson-Gualtieri <thmmth@users.noreply.github.com>

--- pj_bandi_regione_lombardia/src/pj_bandi_regione_lombardia/rag.py
+++ pj_bandi_regione_lombardia/src/pj_bandi_regione_lombardia/rag.py
@@ -22,14 +22,30 @@ class RagSystem:
         """
         self.vector_store = None
         self.loader = None
-        self.text_splitter = None
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
         self._prompt = PromptTemplate(
             input_variables=["question", "context"],
-            template="""
-            You are a helpful assistant. Use the context provided to answer the question.
+            template = """
+            You are an AI assistant specialized in funding opportunities provided by the Lombardy Region.
+            Your goal is to assist businesses and professionals by providing clear, accurate, and useful information about available grants, eligibility criteria, deadlines, allocated funds, and how to apply.
+
+            Use the documents provided by the system (retrieved_documents) to answer with accuracy and clarity.
+            Do not make up information. If the documents do not contain a direct answer, kindly suggest how to obtain official information.
+
+            
+            Tone: professional but understandable. Use clear and simple language.
+            If the question is vague, kindly ask the user to provide more specific details.
+            
+            Respond in standard Italian.
             Question: {question}
             Context: {context}
-            Answer:"""
+            Answer:
+            """
+
         )
         print(api_key, api_version, embedding_model, llm_model)
         #initilize the embeddings model
@@ -67,42 +83,49 @@ class RagSystem:
             return TextLoader(doc_path)
         else:
             raise ValueError("Unsupported file type. Please provide a PDF or TXT file.")
-        
-    def add_data_file(self,doc_path: str):
+    
+    def _create_vector_store(self, doc_path: str):
+        self.loader = self._select_loader(doc_path)
+        documents = self.loader.load_and_split(text_splitter=self.text_splitter)
+        self.vector_store = FAISS.from_documents(
+            documents=documents,
+            embedding=self.embeddings,
+        )
+        print("Vector store created.")
+
+    def _add_data_file(self,doc_path: str):
         """
         Load and process the document file such as pdf or txt, splitting it into chunks and storing embeddings.
         Args:
             doc_path (str): Path to the document file to be processed.
         """
-        if self.vector_store is None:
-            #Select loader based on file type
-            self.loader = self._select_loader(doc_path)
-        
-            # Initialize the text splitter
-            self.text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size=1000,
-                chunk_overlap=200
-            )
+        # If vector store already exists, just load the document and split it into chunks
+        self.loader = self._select_loader(doc_path)
+        documents = self.loader.load_and_split(text_splitter=self.text_splitter)
+        print(f"Split blog post into {len(documents)} sub-documents.")              #TODO remove this print statement
 
-            # Load the document and split it into chunks
-            documents = self.loader.load_and_split(text_splitter=self.text_splitter)
-            print(f"Split blog post into {len(documents)} sub-documents.")
+        self.vector_store.add_documents(documents)
+
+    def add_data_files(self, folder_path: List[str]):
+        """
+        Load and process multiple document files from a specified folder, splitting them into chunks and storing embeddings.
+        Args:
+            folder_path (List[str]): List of paths to the document files to be processed.
+        """
+
+        #list al documents in the folder
+        doc_list = [ os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+        for doc_path in doc_list:
+            if self.vector_store is None:
+                self._create_vector_store(doc_path)
+            else: 
+                if isinstance(doc_path, str):
+                    self._add_data_file(doc_path)
+                else:
+                    raise ValueError("Each document path must be a string.")
+            rag.save_vector_store(vector_store_path=folder_path)
 
 
-            # Store the embeddings in the vector store
-            self.vector_store = FAISS.from_documents(
-                documentsA,
-                self.embeddings
-            )
-            
-        else:
-            # If vector store already exists, just load the document and split it into chunks
-            self.loader = self._select_loader(doc_path)
-            documents = self.loader.load_and_split(text_splitter=self.text_splitter)
-            print(f"Split blog post into {len(documents)} sub-documents.")
-
-            self.vector_store.add_documents(documents)
-    
     def save_vector_store(self, vector_store_path: str):
         """
         Save the vector store to a specified path.
@@ -135,9 +158,57 @@ class RagSystem:
 
         # Generate an answer using the LLM
         answer = self.llm.invoke(prompt)
-
-        print(f"Generated answer: {answer}")
         
         return answer
-
+    def load_vector_store(self, vector_store_path: str):
+        """
+        Load the vector store from a specified path.
+        Args:
+            vector_store_path (str): Path from where the vector store will be loaded.
+        """
+        if self.vector_store is None:
+            self.vector_store = FAISS.load_local(vector_store_path, self.embeddings,allow_dangerous_deserialization=True)
+            print(f"Vector store loaded from {vector_store_path}.")
+        else:
+            print("Vector store is already initialized.")
     
+if __name__ == "__main__":
+
+
+    LOAD_VECTOR_STORE = True
+    # Example usage
+    from dotenv import load_dotenv
+    import os
+    import pathlib
+    load_dotenv()
+
+    api_key = os.getenv("AZURE_API_KEY")
+    api_end_point = os.getenv("AZURE_API_BASE")
+    api_version = os.getenv("AZURE_API_VERSION")
+    embedding_model = os.getenv("AZURE_EMBEDDING_MODEL")
+    llm_model = os.getenv("AZURE_LLM_MODEL")
+
+    rag = RagSystem(
+        api_key=api_key,
+        api_end_point=api_end_point,
+        api_version=api_version,
+        embedding_model=embedding_model,
+        llm_model=llm_model
+    )
+    base_dir = pathlib.Path(__file__).parent.parent
+    db_folder = base_dir / "db"
+    if not(LOAD_VECTOR_STORE):
+        print("Vector store is not initialized. Adding data files...")
+        # Add a document file to the RAG system
+        # Get the absolute path to the "db" folder under "src"
+
+        rag.add_data_files(folder_path=db_folder)
+
+    else: 
+        print("Vector store is already initialized.")
+        rag.load_vector_store(vector_store_path=db_folder)
+
+
+    answer = rag.generate("Riassumi il bando ricircolo STEP e fammi capire come accedere ai benefici previsti in poche parole")
+    print(answer.content)
+
